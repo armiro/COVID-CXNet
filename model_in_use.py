746b82f764ac7123eabd4a2c31420a3be2bae9ce
armiro <arman.hgh.1374@gmail.com>
2020-04-03 00:51:31 -0600

    add model loading segment

--- model_in_use.py
+++ model_in_use.py
@@ -1,8 +1,7 @@
 # from main import build_model, get_last_weights
-from keras.layers import Input, Conv2D, Dense, Flatten
-from keras import Model
+from keras.models import model_from_json, load_model
 from keras.preprocessing.image import load_img, img_to_array
-from vis.visualization import visualize_cam, overlay
+from vis.visualization import visualize_cam
 import numpy as np, glob, cv2, matplotlib.pyplot as plt, matplotlib.cm as cm
 
 
@@ -14,7 +13,7 @@ def get_weights(folder):
     """
     num_epochs = list()
     for weights_file in glob.glob(folder + '/**.hdf5'):
-        num_epoch = int(weights_file[weights_file.find('=')+1:weights_file.find('_')])
+        num_epoch = int(weights_file[weights_file.find('=')+1:weights_file.rfind('_')])
         num_epochs.append((num_epoch, weights_file))
 
     last_file = max(num_epochs)[1]
@@ -22,21 +21,7 @@ def get_weights(folder):
     return last_file
 
 
-def build_model(input_shape):
-    a0 = Input(shape=input_shape, name='input_layer')
-    a1 = Conv2D(filters=32, kernel_size=5, strides=2, padding='valid', activation='relu', name='conv_layer1')(a0)
-    a2 = Conv2D(filters=32, kernel_size=3, strides=2, padding='valid', activation='relu', name='conv_layer2')(a1)
-    a3 = Conv2D(filters=32, kernel_size=3, strides=2, padding='valid', activation='relu', name='conv_layer3')(a2)
-    a4 = Conv2D(filters=32, kernel_size=3, strides=2, padding='valid', activation='relu', name='conv_layer4')(a3)
-    a5 = Conv2D(filters=32, kernel_size=3, strides=2, padding='valid', activation='relu', name='conv_layer5')(a4)
-    a6 = Flatten(name='flatten_layer')(a5)
-    a7 = Dense(units=10, activation='relu', name='fc_layer1')(a6)
-    a8 = Dense(units=10, activation='relu', name='fc_layer2')(a7)
-    a9 = Dense(units=1, activation='sigmoid', name='output_layer')(a8)
-    return Model(inputs=a0, outputs=a9, name='binary_classifier')
-
-
-IMG_PATH = ['./chest_xray_images/covid19/016.jpeg', 1]
+IMG_PATH = ['./chest_xray_images/covid19/020.jpg', 1]
 
 test_img = load_img(path=IMG_PATH[0], color_mode='grayscale')
 test_img = img_to_array(img=test_img, data_format='channels_last')
@@ -45,16 +30,24 @@ test_img = test_img / 255.
 test_img = np.expand_dims(test_img, axis=-1)
 print('external image(s) shape:', test_img.shape)
 
-predictor = build_model(input_shape=(400, 400, 1))
-predictor.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-predictor.load_weights(get_weights('./checkpoints'))
+# load model as a json file and load weights from .hdf5 file
+json_file = open(file='./checkpoints/base_model/base_model.json', mode='r')
+model_json = json_file.read()
+json_file.close()
+predictor = model_from_json(model_json)
+predictor.load_weights(get_weights('./checkpoints/base_model'))
+
+# load_model if the model is saved as a single .h5 file
+# predictor = tf.keras.models.load_model('./checkpoints/teachable_machine_google/keras_model.h5')
+# predictor.summary()
 
 grads = visualize_cam(predictor, layer_idx=-1, filter_indices=None, seed_input=test_img, backprop_modifier=None,
                       grad_modifier=None, penultimate_layer_idx=None)
 jet_heatmap = np.uint8(cm.jet(grads)[:, :, :, 0] * 255)
 
 # test_img_3ch = np.concatenate((test_img, test_img, test_img), axis=-1)
-# plt.imshow(overlay(jet_heatmap, test_img_3ch, alpha=0.4))
+# out = cv2.addWeighted(src1=np.uint8(test_img_3ch * 255), alpha=0.8, src2=jet_heatmap, beta=0.2, gamma=0)
+
 fig = plt.figure()
 plt.subplot(1, 2, 1)
 plt.imshow(jet_heatmap)
