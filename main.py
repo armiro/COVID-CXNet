495b2f5927e00e35fe63e845c0c934974b9e25b4
armiro <arman.hgh.1374@gmail.com>
2020-04-04 21:44:31 -0600

    minor changes

--- main.py
+++ main.py
@@ -1,13 +1,55 @@
 import os, glob, numpy as np, shutil, matplotlib.pyplot as plt, cv2
-import tensorflow as tf
-import keras
+import tensorflow as tf, keras
+import keras.callbacks as cb
 
 from keras.preprocessing.image import load_img, img_to_array, ImageDataGenerator
 from keras.layers import Input, Conv2D, Dense, Add, Flatten, BatchNormalization, Dropout
-from keras.layers import GlobalAveragePooling2D, MaxPooling2D, Concatenate, Activation
+# from keras.layers import GlobalAveragePooling2D, MaxPooling2D, Concatenate, Activation
 from keras import Model, optimizers
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, classification_report
+
+
+def data_preparation(path):
+    normal_images = list()
+    for img_name in glob.glob(pathname=path + '/normal/*'):
+        img = load_img(path=img_name, color_mode='grayscale')
+        img = img_to_array(img=img, data_format='channels_last')
+        normal_images.append(img)
+
+    normal_images = np.array(normal_images)
+    print('number of normal chest xrays:', len(normal_images))
+
+    covid_images = list()
+    for img_name in glob.glob(pathname=path + '/covid19/*'):
+        img = load_img(path=img_name, color_mode='grayscale')
+        img = img_to_array(img=img, data_format='channels_last')
+        covid_images.append(img)
+
+    covid_images = np.array(covid_images)
+    print('number of covid19 chest xrays:', len(covid_images))
+
+    normal_labels = [0 for _ in range(len(normal_images))]
+    covid_labels = [1 for _ in range(len(covid_images))]
+
+    X = np.concatenate((covid_images, normal_images))
+    y = np.array(covid_labels + normal_labels)
+
+    X = np.array([cv2.resize(image, dsize=(400, 400), interpolation=cv2.INTER_CUBIC) for image in X])
+    X = np.array([np.expand_dims(a=image, axis=-1) for image in X])
+    X = np.array([X[idx] / 255. for idx in range(len(X))])
+
+    print('number of total dataset images:', len(X))
+    print('number of total dataset labels:', len(y))
+    print('dataset shape:', X.shape)
+    rnd_idx = np.random.choice(a=len(X), size=None)
+    plt.imshow(X=X[rnd_idx].squeeze(), cmap='gray')
+    plt.axis('off')
+    plt.title(label='a random image from the dataset')
+    plt.show()
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=18)
+    return X_train, X_test, y_train, y_test
 
 
 def get_last_weights(folder):
@@ -18,7 +60,7 @@ def get_last_weights(folder):
     """
     num_epochs = list()
     for weights_file in glob.glob(folder + '/**.hdf5'):
-        num_epoch = int(weights_file[weights_file.find('=')+1:weights_file.find('_')])
+        num_epoch = int(weights_file[weights_file.find('=')+1:weights_file.rfind('_')])
         num_epochs.append((num_epoch, weights_file))
 
     last_epoch = max(num_epochs)[0]
@@ -71,46 +113,7 @@ print('Keras version is:', keras.__version__)
 
 
 """image data preparation"""
-path = './chest_xray_images/'
-normal_images = list()
-for img_name in glob.glob(pathname=path+'normal/*'):
-    img = load_img(path=img_name, color_mode='grayscale')
-    img = img_to_array(img=img, data_format='channels_last')
-    normal_images.append(img)
-
-normal_images = np.array(normal_images)
-print('number of normal chest xrays:', len(normal_images))
-
-covid_images = list()
-for img_name in glob.glob(pathname=path+'/covid19/*'):
-    img = load_img(path=img_name, color_mode='grayscale')
-    img = img_to_array(img=img, data_format='channels_last')
-    covid_images.append(img)
-
-covid_images = np.array(covid_images)
-print('number of covid19 chest xrays:', len(covid_images))
-
-normal_labels = [0 for _ in range(len(normal_images))]
-covid_labels = [1 for _ in range(len(covid_images))]
-
-X = np.concatenate((covid_images, normal_images))
-y = np.array(covid_labels + normal_labels)
-
-X = np.array([cv2.resize(image, dsize=(400, 400), interpolation=cv2.INTER_CUBIC) for image in X])
-X = np.array([np.expand_dims(a=image, axis=-1) for image in X])
-X = np.array([X[idx] / 255. for idx in range(len(X))])
-
-print('number of total dataset images:', len(X))
-print('number of total dataset labels:', len(y))
-print('dataset shape:', X.shape)
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=18)
-
-rnd_idx = np.random.choice(a=len(X), size=None)
-plt.imshow(X=X[rnd_idx].squeeze(), cmap='gray')
-plt.axis('off')
-plt.title(label='a random image from the dataset')
-plt.show()
+X_train, X_test, y_train, y_test = data_preparation(path='./chest_xray_images/')
 
 
 """data augmentation using keras"""
@@ -119,13 +122,13 @@ test_augmenter = ImageDataGenerator(rescale=None)
 
 
 """model callbacks"""
-checkpoint = keras.callbacks.ModelCheckpoint(filepath='./checkpoints/eps={epoch:03d}_valAcc={val_accuracy:.4f}.hdf5',
-                                             monitor='val_accuracy', verbose=1, save_best_only=True, mode='max')
+checkpoint = cb.ModelCheckpoint(filepath='./checkpoints/base_model/v1.1/eps={epoch:03d}_valAcc={val_accuracy:.4f}.hdf5',
+                                monitor='val_accuracy', verbose=1, save_best_only=True, mode='max')
 cb_list = [checkpoint]
 
 
 """create the classifier model"""
-classifier = build_model(input_shape=X[0].shape)
+classifier = build_model(input_shape=X_train[0].shape)
 classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 print(classifier.summary())
 print('number of network layers:', len(classifier.layers))
@@ -140,11 +143,11 @@ plt.plot(training.history['loss'], color='r', label='training_loss')
 plt.plot(training.history['val_loss'], color='g', label='validation_loss')
 plt.legend()
 plt.show()
-fig.savefig('./training_history.png')
+fig.savefig('./checkpoints/base_model/v1.1/training_history.png')
 
 
 """best results on the test-set"""
-weights_folder = './checkpoints'
+weights_folder = './checkpoints/base_model/v1.1'
 _, best_weights = get_last_weights(weights_folder)
 acc = float(best_weights[best_weights.rfind('=')+1:best_weights.rfind('.')])
 print('best validation accuracy:', acc)
@@ -160,4 +163,10 @@ y_pred_rnd = np.round(np.reshape(a=y_pred, newshape=(1, -1)))[0]
 cm = confusion_matrix(y_true=y_test, y_pred=y_pred_rnd)
 print(cm)
 print('accuracy:', (cm[0][0] + cm[1][1])/np.sum(cm))
+
+
+"""save the model to a json file"""
+model_json = classifier.to_json()
+with open("./checkpoints/base_model/v1.1/base_model.json", "w") as json_file:
+    json_file.write(model_json)
 
