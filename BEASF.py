f3eaf45baa606a10acc56d8c511c73dcf759f717
armiro <arman.hgh.1374@gmail.com>
2020-04-07 21:03:22 -0600

    cross-validate result with the ref paper, add credit

--- BEASF.py
+++ BEASF.py
@@ -3,24 +3,45 @@ import copy
 
 
 def subhist(image_pdf, minimum, maximum, normalize):
-    minimum = np.int32(minimum)
-    maximum = np.int32(maximum+1)
+    """
+    Compute the subhistogram between [minimum, maximum] of a given histogram image_pdf
+    :param image_pdf: numpy.array
+    :param minimum: int
+    :param maximum: int
+    :param normalize: boolean
+    :return: numpy.array
+    """
     hi = np.zeros(shape=image_pdf.shape)
     total = 0
-    for idx in range(minimum, maximum):
+    for idx in range(minimum, maximum+1):
         total += image_pdf[idx]
         hi[idx] = image_pdf[idx]
     if normalize:
-        for idx in range(minimum, maximum):
+        for idx in range(minimum, maximum+1):
             hi[idx] /= total
     return hi
 
 
+def CDF(hist):
+    """
+    Compute the CDF of the input histogram
+    :param hist: numpy.array()
+    :return: numpy.array()
+    """
+    cdf = np.zeros(shape=hist.shape)
+    cdf[0] = hist[0]
+    for idx in range(1, len(hist)):
+        cdf[idx] = cdf[idx - 1] + hist[idx]
+    return cdf
+
+
 def BEASF(image, gamma):
     """
-    Computes the BEASF algorithm; built using Numpy package
+    Compute the Bi-Histogram Equalization with Adaptive Sigmoid Functions algorithm (BEASF)
     A python implementation of the original MATLAB code:
-    The algorithm is introduced by XXX, in the research paper: XXX
+    https://mathworks.com/matlabcentral/fileexchange/47517-beasf-image-enhancer-for-gray-scale-images
+    The algorithm is introduced by E. F. Arriaga-Garcia et al., in the research paper:
+    https://ieeexplore.ieee.org/document/6808563
     :param image: numpy.ndarray
     :param gamma: float [0, 1]
     :return: numpy.ndarray
@@ -30,56 +51,46 @@ def BEASF(image, gamma):
     h_lower = subhist(image_pdf=h, minimum=0, maximum=m, normalize=True)
     h_upper = subhist(image_pdf=h, minimum=m, maximum=255, normalize=True)
 
-    # cdf_lower = norm.cdf(h_lower)
-    cdf_lower = np.zeros(shape=h_lower.shape)
-    cdf_lower[0] = h_lower[0]
-    for idx in range(1, len(h_lower)):
-        cdf_lower[idx] = cdf_lower[idx - 1] + h_lower[idx]
+    cdf_lower = CDF(hist=h_lower)
+    cdf_upper = CDF(hist=h_upper)
 
-    # cdf_upper = norm.cdf(h_upper)
-    cdf_upper = np.zeros(shape=h_upper.shape)
-    cdf_upper[0] = h_upper[0]
-    for idx in range(1, len(h_upper)):
-        cdf_upper[idx] = cdf_upper[idx - 1] + h_upper[idx]
-
+    # Find x | CDF(x) = 0.5
     half_low = 0
-    for idx in range(0, int(m)):
-        if cdf_lower[idx + 1] > 0.5:
+    for idx in range(0, m+2):
+        if cdf_lower[idx] > 0.5:
             half_low = idx
             break
     half_up = 0
-    for idx in range(int(m), 256):
+    for idx in range(m, 256):
         if cdf_upper[idx + 1] > 0.5:
             half_up = idx
             break
 
     # sigmoid CDF creation
-    tones_low = np.arange(0, m, 1)
-    x_low = 5.0 * (tones_low - half_low) / m  # shift & scale intensity x to place sigmoid [-2.5, 2.5
-    # lower sigmoid
-    s_low = 1 / (1 + np.exp(-gamma * x_low))
+    tones_low = np.arange(0, m+1, 1)
+    x_low = 5.0 * (tones_low - half_low) / m  # shift & scale intensity x to place sigmoid [-2.5, 2.5]
+    s_low = 1 / (1 + np.exp(-gamma * x_low))  # lower sigmoid
 
-    tones_up = np.arange(m, 255, 1)
-    x_up = 5.0 * (tones_up - half_up) / (255 - m)  # shift & scale intensity x to place sigmoid [-2.5, 2.5
-    # upper sigmoid
-    s_up = 1 / (1 + np.exp(-gamma * x_up))
+    tones_up = np.arange(m, 256, 1)
+    x_up = 5.0 * (tones_up - half_up) / (255 - m)  # shift & scale intensity x to place sigmoid [-2.5, 2.5]
+    s_up = 1 / (1 + np.exp(-gamma * x_up))  # upper sigmoid
 
     mapping_vector = np.zeros(shape=(256,))
-    for idx in range(0, m):
+    for idx in range(0, m+1):
         mapping_vector[idx] = np.int32(m * s_low[idx])
 
     minimum = mapping_vector[0]
     maximum = mapping_vector[m]
-    for idx in range(0, m):
+    for idx in range(0, m+1):
         mapping_vector[idx] = np.int32((m / (maximum - minimum)) * (mapping_vector[idx] - minimum))
-    for idx in range(m + 1, 256):
+    for idx in range(m+1, 256):
         mapping_vector[idx] = np.int32(m + (255 - m) * s_up[idx - m - 1])
 
     minimum = mapping_vector[m + 1]
     maximum = mapping_vector[255]
-    for idx in range(m + 1, 256):
+    for idx in range(m+1, 256):
         mapping_vector[idx] = (255 - m) * (mapping_vector[idx] - minimum) / (maximum - minimum) + m
 
-    result = copy.deepcopy(image)
-    result[:, :] = mapping_vector[image[:, :]]
-    return result
+    res = copy.deepcopy(image)
+    res[:, :] = mapping_vector[image[:, :]]
+    return res
