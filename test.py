25a7ddf61f2b604416745280ecd128f8d9092386
armiro <arman.hgh.1374@gmail.com>
2020-04-30 03:47:38 -0600

    initial result by CheXNet!

--- test.py
+++ test.py
@@ -1,55 +1,28 @@
-# test file
 from keras.preprocessing.image import load_img, img_to_array
-import cv2.cv2 as cv2, numpy as np, matplotlib.pyplot as plt
-from skimage.filters import rank
-from skimage.morphology import disk
-from BEASF import BEASF
+import cv2.cv2 as cv2, numpy as np
+from keras.applications import DenseNet121
+from keras.layers import Dense, GlobalAveragePooling2D
+from keras import Model
 
-IMG_PATH = ['./chest_xray_images/covid19/188.jpg', '1']
-IMG_SHAPE = (500, 500, 1)
+
+IMG_PATH = ['./chest_xray_images/normal/15268.jpg', '0']
+IMG_SHAPE = (320, 320, 3)
 
 test_img = load_img(path=IMG_PATH[0], color_mode='grayscale')
 test_img = img_to_array(img=test_img, data_format='channels_last')
 test_img = cv2.resize(test_img, dsize=IMG_SHAPE[:2], interpolation=cv2.INTER_NEAREST)
+test_img = np.expand_dims(test_img, axis=-1)
 test_img = test_img.astype(np.uint8)
+test_img = test_img / 255.
+test_img = np.concatenate((test_img, test_img, test_img), axis=-1)
+print('external image(s) shape:', test_img.shape)
 
-clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
-test_img_clahe = clahe.apply(test_img)
-# test_img_ahe = rank.equalize(image=test_img, selem=disk(150))
-# test_img_equ = cv2.equalizeHist(test_img)
-#
-# fig = plt.figure()
-# plt.subplot(2, 2, 1)
-# plt.imshow(test_img, cmap='gray')
-# plt.axis('off')
-# plt.title('org')
-# plt.subplot(2, 2, 2)
-# plt.imshow(test_img_equ, cmap='gray')
-# plt.axis('off')
-# plt.title('HE')
-# plt.subplot(2, 2, 3)
-# plt.imshow(test_img_ahe, cmap='gray')
-# plt.axis('off')
-# plt.title('AHE')
-# plt.subplot(2, 2, 4)
-# plt.imshow(test_img_clahe, cmap='gray')
-# plt.axis('off')
-# plt.title('CLAHE')
-# plt.show()
-# # fig.savefig(fname='./hist_equal14.png')
+backbone = DenseNet121(include_top=False, weights=None, input_shape=(320, 320, 3))
+backbone_out = backbone.output
+gap = GlobalAveragePooling2D(name='pooling_layer')(backbone_out)
+output = Dense(units=14, activation='sigmoid', name='output_layer')(gap)
+predictor = Model(inputs=backbone.input, outputs=output)
+print(predictor.summary())
+predictor.load_weights('C:/Users/Arman/Desktop/Covid19-Detection/checkpoints/CheXNet/CheXNet_v0.3.0.h5')
+print(predictor.predict(np.expand_dims(test_img, axis=0)))
 
-fig = plt.figure()
-for i in range(0, 6):
-    plt.subplot(2, 3, i+1)
-    if i == 0:
-        plt.imshow(X=test_img, cmap='gray')
-        plt.title('org img')
-    elif i == 5:
-        plt.imshow(X=test_img_clahe, cmap='gray')
-        plt.title('CLAHE')
-    else:
-        plt.imshow(X=BEASF(image=test_img, gamma=(i/2)), cmap='gray')
-        plt.title('BEASF (gamma=%.1f)' % (i / 2))
-    plt.axis('off')
-plt.show()
-# fig.savefig(fname='./beasf6.png')
