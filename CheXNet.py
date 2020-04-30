d3e08529eb8a9c44ddb01e5ec08452a001b94136
armiro <arman.hgh.1374@gmail.com>
2020-04-30 13:41:15 -0600

    chexnet model definition and weights import

---
+++ CheXNet.py
@@ -0,0 +1,31 @@
+from keras.preprocessing.image import load_img, img_to_array
+import cv2.cv2 as cv2, numpy as np
+from keras.applications import DenseNet121
+from keras.layers import Dense, GlobalAveragePooling2D
+from keras import Model
+from keras.models import save_model
+
+
+IMG_PATH = ['./chest_xray_images/normal/15268.jpg', '0']
+IMG_SHAPE = (320, 320, 3)
+
+test_img = load_img(path=IMG_PATH[0], color_mode='grayscale')
+test_img = img_to_array(img=test_img, data_format='channels_last')
+test_img = cv2.resize(test_img, dsize=IMG_SHAPE[:2], interpolation=cv2.INTER_NEAREST)
+test_img = np.expand_dims(test_img, axis=-1)
+test_img = test_img.astype(np.uint8)
+test_img = test_img / 255.
+test_img = np.concatenate((test_img, test_img, test_img), axis=-1)
+print('external image(s) shape:', test_img.shape)
+
+backbone = DenseNet121(include_top=False, weights=None, input_shape=(320, 320, 3))
+backbone_out = backbone.output
+gap = GlobalAveragePooling2D(name='pooling_layer')(backbone_out)
+output = Dense(units=14, activation='sigmoid', name='output_layer')(gap)
+chexnet_model = Model(inputs=backbone.input, outputs=output)
+print(chexnet_model.summary())
+chexnet_model.load_weights('C:/Users/Arman/Desktop/Covid19-Detection/checkpoints/CheXNet/CheXNet_v0.3.0.h5')
+# save_model(model=chexnet_model, filepath='./checkpoints/CheXNet/CheXNet_model.hdf5', overwrite=True)
+chexnet_model.save(filepath='./checkpoints/CheXNet/CheXNet_model.hdf5')
+print(chexnet_model.predict(np.expand_dims(test_img, axis=0)))
+
