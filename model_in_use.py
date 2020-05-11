6a07e1dbe6f2c5778085f91729427b9319cdfe00
armiro <arman.hgh.1374@gmail.com>
2020-05-10 22:34:56 -0600

    minor changes

--- model_in_use.py
+++ model_in_use.py
@@ -1,4 +1,4 @@
-# from main import build_model, get_last_weights
+# from main import base_model, get_last_weights
 from tensorflow.keras.models import model_from_json, load_model
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 import numpy as np, glob, cv2.cv2 as cv2, matplotlib.pyplot as plt, copy
@@ -48,42 +48,42 @@ test_img = test_img / 255.
 print('external image(s) shape:', test_img.shape)
 
 # load model as a json file and load weights from .hdf5 file
-json_file = open(file='./checkpoints/CheXNet/COVID-CXNet_model.json', mode='r')
+json_file = open(file='./checkpoints/COVID-CXNet/COVID-CXNet_model.json', mode='r')
 model_json = json_file.read()
 json_file.close()
 cxnet = model_from_json(model_json)
-cxnet.load_weights('./checkpoints/CheXNet/eps=008_valLoss=0.0311.hdf5')
+cxnet.load_weights('./checkpoints/COVID-CXNet/eps=008_valLoss=0.0311.hdf5')
 
 # load_model if the model is saved as a single .h5 file
-# chexnet = load_model('./checkpoints/CheXNet/CheXNet_model.hdf5')
+chexnet = load_model('./checkpoints/CheXNet/CheXNet_model.hdf5')
 
-cam = GradCAM(model=cxnet, classIdx=0, layerName=None)
+cam = GradCAM(model=chexnet, classIdx=6, layerName=None)
 heatmap = cam.compute_heatmap(image=test_img, normalize=True)
-overlaid_heatmap = cam.overlay_heatmap(heatmap=heatmap, image=ref_img, alpha=0.8, colormap=cv2.COLORMAP_HSV)
+overlaid_heatmap = cam.overlay_heatmap(heatmap=heatmap, image=temp_img, alpha=0.8, colormap=cv2.COLORMAP_HSV)
 
 fig1 = plt.figure()
 plt.subplot(1, 2, 1)
 plt.imshow(overlaid_heatmap)
 plt.axis('off')
-plt.title('pred=%.4f' % cxnet.predict(np.expand_dims(test_img, axis=0)))
+plt.title('pred=%.4f' % chexnet.predict(np.expand_dims(test_img, axis=0))[0][6])
 plt.subplot(1, 2, 2)
 plt.imshow(ref_img.squeeze(), cmap='gray')
 plt.axis('off')
 plt.title('label=%s' % IMG_PATH[1])
 plt.show()
-# fig1.savefig(fname='./?.png')
+# fig1.savefig(fname='./?.svg')
 
-temp, mask = generate_explanation(model=cxnet, input_image=test_img)
+temp, mask = generate_explanation(model=chexnet, input_image=test_img)
 
 fig2 = plt.figure()
 plt.subplot(1, 2, 1)
 plt.imshow(mark_boundaries(temp / 2 + 0.5, mask))
 plt.axis('off')
-plt.title('pred=%.4f' % cxnet.predict(np.expand_dims(test_img, axis=0)))
+plt.title('pred=%.4f' % chexnet.predict(np.expand_dims(test_img, axis=0))[0][6])
 plt.subplot(1, 2, 2)
 plt.imshow(ref_img.squeeze(), cmap='gray')
 plt.axis('off')
 plt.title('label=%s' % IMG_PATH[1])
 plt.show()
-# fig1.savefig(fname='./?.png')
+# fig1.savefig(fname='./?.svg')
 
